1855ad192ced4052e16fb02f50794a3706ab503f
AlessandroMIlani <alessandromilani2000@gmail.com>
2022-09-04 17:23:49 +0200

    add font function

--- myFunctions.py
+++ myFunctions.py
@@ -1,9 +1,11 @@
 from AnilistPython import Anilist
 import random
+import time
 
 
 def get_anime(genre_list, years, vote):
     anilist = Anilist()
+    print("vote:", vote)
     # Get anime list
     if years[0] == years[1] or years[0] > years[1]:
         print("Error: invalid year range")
@@ -11,10 +13,20 @@ def get_anime(genre_list, years, vote):
         random_anime = anilist.search_anime(genre=genre_list, score=range(int(vote), 101))
     else:
         # Get random anime
-        random_anime = anilist.search_anime(genre=genre_list, score=range(int(vote), 101), year=years)
+        random_anime = anilist.search_anime(genre=genre_list, year=[years[0], years[1]], score=range(int(vote), 100))
     # if random_anime is empty, get another one
     if len(random_anime) != 0:
-        picked = random.choice(random_anime)
+        picked = random.Random(int(round(time.time() * 1000))).choice(random_anime)
         anilist.print_anime_info(picked["name_english"])
+        return picked
     else:
         print("No anime found")
+        return None
+        
+        
+
+def get_font_markup(fontdesc, text):
+    return f'<span font_desc="{fontdesc}">{text}</span>'
+    
+    
+    
